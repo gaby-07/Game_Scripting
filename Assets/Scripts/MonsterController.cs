f820d2f6f737228b99633bec8f2005bbcab3c4a1
gaby-07 <61672478+gaby-07@users.noreply.github.com>
2020-04-24 23:07:02 -0500

    Changes to scene and added prefabs

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float speed = 10f;
+    private float xRange = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 15.0f;
-    private float turnSpeed = 30.0f;
+    private float speed = 40.0f;
+    private float turnSpeed = 50.0f;
     private float horizontalInput;
     private float forwardInput;
 
